46ff99a8808470ae5afd9a556f3996d08385e647
vftens <aico@ya.ru>
2022-10-14 21:03:08 +0300

    New commit 2022 Searl Disk 2 running kate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+// которая на входе принимает число, и выдает из всех  чисел от 1 до этого 99, наибольшую цифру из этого числа.
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+first=a/10;
+second=a%10;
+if (first > second){
+    Console.WriteLine(first);
+}
+else{
+    Console.WriteLine(second);
+}
