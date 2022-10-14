de54c75ea6298ce5b5014966f06f556fee5ce5d8
vftens <aico@ya.ru>
2022-10-14 13:52:10 +0300

    New commit 2022 Searl Disk numbers execises 5 num running

--- numbers14OCT2022.cs
+++ numbers14OCT2022.cs
@@ -3,125 +3,122 @@
 
 namespace Application
 {
-    
-
-public class numbers14OCT2022
-{
-    static void Main1(string[] args)
+    public class numbers
     {
-        Console.WriteLine("Введите первое число");
-        int a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Введите второе число");
-        int b = Convert.ToInt32(Console.ReadLine());
-        if (a > b)
+        static void Main1(string[] args)
         {
-            Console.WriteLine("Первое число больше второго");
+            Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("Второе число больше первого");
+            }
+            else
+            {
+                Console.WriteLine("Числа равны");
+            }
         }
-        else if (a < b)
+        //}
+        //{
+
+        //}
+
+        // которая на входе принимает три числа, и выдает максимальное из них.
+
+        //class numbers14OCT2022
+        //{
+        static void Main2(string[] args)
         {
-            Console.WriteLine("Второе число больше первого");
+            Console.WriteLine("Введите п");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите в)");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите с");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int max = a > b ? a : b;
+            if (c > max)
+            {
+                max = c;
+            }
+            /*
+            { 
+
+
+            }
+            else if (c < b)
+            {
+            }
+            else if (a < c)
+            {
+
+            }
+
+            else
+            {
+            }
+            */
+            Console.WriteLine(max);
         }
-        else
+        //}
+
+        // которая на входе принимает число, и выдает является ли оно четным или нечетным. (делится на 2 без остатка или с остатком)
+        //
+
+        //class numbers14OCT2022
+        //{
+        static void Main3(string[] args)
         {
-            Console.WriteLine("Числа равны");
+            Console.WriteLine("Введите п");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+            {
+                //  
+            }
+        }
+        //}
+
+        // которая на входе принимает число, и выдает все чётные числа от 1 до этого числа.
+
+        //class numbers14OCT2022
+        //{
+        static void Main4(string[] args)
+        {
+            Console.WriteLine("Введите п);");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int length = a;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(" " + (i + 1));
+            }
+        }
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+                //numbers num = numbers.Main1();
+                numbers.Main1(null);
+                numbers.Main2(null);
+                numbers.Main3(null);
+                numbers.Main4(null);
+
+            }
         }
     }
-//}
-//{
+
+    //program.Run();
     
-//}
-
-// которая на входе принимает три числа, и выдает максимальное из них.
-
-//class numbers14OCT2022
-//{
-    static void Main2(string[] args)
-    {
-        Console.WriteLine("Введите п");
-        int a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Введите в)");
-        int b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Введите с");
-        int c = Convert.ToInt32(Console.ReadLine());
-        int max = a > b ? a : b;
-        if (c > max)
-        {
-            max = c;
-        }
-        /*
-        { 
-
-
-        }
-        else if (c < b)
-        {
-        }
-        else if (a < c)
-        {
-
-        }
-
-        else
-        {
-        }
-        */
-        Console.WriteLine(max);
-    }
-//}
-
-// которая на входе принимает число, и выдает является ли оно четным или нечетным. (делится на 2 без остатка или с остатком)
-//
-
-//class numbers14OCT2022
-//{
-    static void Main3(string[] args)
-    {
-        Console.WriteLine("Введите п");
-        int a = Convert.ToInt32(Console.ReadLine());
-        if (a % 2 == 0)
-        {
-            Console.WriteLine("Число четное");
-        }
-        else
-        {
-            Console.WriteLine("Число нечетное");
-        }
-        {
-            //  
-        }
-    }
-//}
-
-// которая на входе принимает число, и выдает все чётные числа от 1 до этого числа.
-
-//class numbers14OCT2022
-//{
-    static void Main4(string[] args)
-    {
-        Console.WriteLine("Введите п);");
-        int a = Convert.ToInt32(Console.ReadLine());
-        int length = a;
-        for (int i = 0; i < length; i++)
-        {
-            Console.WriteLine(" "+ (i+1));
-        }
-    }
-}
-
-//program.Run();
-class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-    
-    
-//{    
-    numbers14OCT2022.Main1();
-    Main2();
-    Main3();
-    Main4();
-
-        }
-        }
 }
