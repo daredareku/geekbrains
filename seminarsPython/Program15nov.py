8f3e44584657896815ae7aa699c163955a002637
vftens <aico@ya.ru>
2022-11-21 21:05:23 +0300

    New commit 2022 Searl Disk 21n.1 running kate

--- seminarsPython/Program15nov.py
+++ seminarsPython/Program15nov.py
@@ -5,3 +5,22 @@
 
 # С помощью использования **лямбд, filter, map, zip, enumerate, list comprehension
 # В этом случае можно пропустить совсем тривиальные (т.е. задачу 1 или 2 тут точно решать не имеет смысла) - исходите из уровня группы и студента.
+lst=list(map(int, input().split()))
+ukicum=list(set(lst)) #.difference()
+print(ukicum)
+
+lst=input().split()
+result_list=list(filter(None, lst))
+
+lst = list(map(int, input().split()))
+unikum = list(set(lst)) 
+print(unikum)  
+
+lst = input().split()
+result_list = list(filter(lambda x: (lst.count(x) == 1), lst))
+print(result_list)      
+
+exp = str(input())
+res = eval(exp)
+print(res)
+
---
+++ seminarsPython/outcalc.py
@@ -0,0 +1,6 @@
+#
+#import calc
+from calc import *
+from inp import *
+myOutput(calculate(
+myInput(), myInput(), myInput()))
